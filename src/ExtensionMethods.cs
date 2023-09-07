d25622bd3aea01f58a783084acf723c668baeb51
Austin Vandersluis <austinv900@nindroidservers.com>
2023-09-06 18:54:57 -0700

    Add extension methods to query assembly for metadata

---
+++ src/ExtensionMethods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Oxide
+{
+    public static class ExtensionMethods
+    {
+        #region Metadata
+
+        /// <summary>
+        /// Gets all <see cref="AssemblyMetadataAttribute"/> from a assembly
+        /// </summary>
+        /// <param name="assembly">The assembly to query for metadata</param>
+        /// <returns>Array of metadata</returns>
+        public static AssemblyMetadataAttribute[] Metadata(this Assembly assembly)
+        {
+            return Attribute.GetCustomAttributes(assembly, typeof(AssemblyMetadataAttribute), false) as AssemblyMetadataAttribute[];
+        }
+
+        /// <summary>
+        /// Gets values of all <see cref="AssemblyMetadataAttribute"/> filtered by key
+        /// </summary>
+        /// <param name="assembly">The assembly to query for metadata</param>
+        /// <param name="key">The metadata key to filter by</param>
+        /// <returns>Array of metadata values</returns>
+        public static string[] Metadata(this Assembly assembly, string key)
+        {
+            return assembly.Metadata().Where(meta => meta.Key.Equals(key, StringComparison.InvariantCultureIgnoreCase)).Select(meta => meta.Value).ToArray();
+        }
+
+        #endregion
+    }
+}
