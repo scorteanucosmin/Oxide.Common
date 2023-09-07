015b4af7bd361318f63aefaa3cbeda0b2138502d
Austin Vandersluis <austinv900@nindroidservers.com>
2023-09-06 17:59:58 -0700

    Add AssemblyMetadataAttribute for NET35 and NET40

---
+++ src/Stubs/AssemblyMetadataAttribute.cs
@@ -0,0 +1,20 @@
+﻿#if NET35 || NET40
+
+namespace System.Reflection
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public class AssemblyMetadataAttribute : Attribute
+    {
+        public string Key { get; protected set; }
+
+        public string Value { get; protected set; }
+
+        public AssemblyMetadataAttribute(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
+
+#endif
