257aa05cc75a9f698d0be989636b85a3e6ed89a4
Austin Vandersluis <austinv900@nindroidservers.com>
2023-10-15 17:49:04 -0700

    Add pooling providers [skip ci]

---
+++ src/Pooling/IArrayPoolProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace Oxide.Pooling
+{
+    /// <inheritdoc cref="IPoolProvider"/>
+    /// <typeparam name="T">The item type this pool manages</typeparam>
+    public interface IArrayPoolProvider<out T> : IPoolProvider<T[]>
+    {
+        /// <summary>
+        /// Takes a single <see cref="T"/> array from this pool
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns><see cref="T"/>[]</returns>
+        T[] Take(int length);
+    }
+}
