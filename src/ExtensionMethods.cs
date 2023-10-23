6071164a62ec0eba9bc0b57a1962e88d9a4e2d16
Austin Vandersluis <austinv900@nindroidservers.com>
2023-10-22 19:03:50 -0700

    Add some extension methods for collections
    Some methods missing from earlier versions of .NET

--- src/ExtensionMethods.cs
+++ src/ExtensionMethods.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -28,6 +29,82 @@ namespace Oxide
         {
             return assembly.Metadata().Where(meta => meta.Key.Equals(key, StringComparison.InvariantCultureIgnoreCase)).Select(meta => meta.Value).ToArray();
         }
+
+        #endregion
+
+        #region Enumberables
+
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> collection)
+        {
+            HashSet<T> set = new HashSet<T>();
+
+            foreach (T item in collection)
+            {
+                set.Add(item);
+            }
+
+            return set;
+        }
+
+        #endregion
+
+        #region Collections
+
+#if NETFRAMEWORK || NETSTANDARD2_0
+
+        // Stack<T>
+        public static bool TryPop<T>(this Stack<T> stack, out T value)
+        {
+            if (stack.Count > 0)
+            {
+                value = stack.Pop();
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+
+        // Stack<T>
+        public static bool TryPeek<T>(this Stack<T> stack, out T value)
+        {
+            if (stack.Count > 0)
+            {
+                value = stack.Peek();
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+
+        // Queue<T>
+        public static bool TryDequeue<T>(this Queue<T> queue, out T value)
+        {
+            if (queue.Count > 0)
+            {
+                value = queue.Dequeue();
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+
+        // Queue<T>
+        public static bool TryPeek<T>(this Queue<T> queue, out T value)
+        {
+            if (queue.Count > 0)
+            {
+                value = queue.Peek();
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+
+#endif
 
         #endregion
     }
