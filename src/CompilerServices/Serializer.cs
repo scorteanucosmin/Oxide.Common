86d9b9fc2be39e040c5011839b78c82cf1c10bca
scorteanucosmin <cozminsc@gmail.com>
2026-02-11 23:07:54 +0200

    Update Serializer to not emit BOM

--- src/CompilerServices/Serializer.cs
+++ src/CompilerServices/Serializer.cs
@@ -9,25 +9,27 @@ namespace Oxide.CompilerServices;
 public class Serializer : ISerializer
 {
     private readonly JsonSerializer _jsonSerializer;
+    private readonly UTF8Encoding _encoding;
 
     public Serializer()
     {
         _jsonSerializer = new JsonSerializer();
+        _encoding = new UTF8Encoding(false);
     }
 
     public byte[] Serialize<T>(T type) where T : class
     {
         using MemoryStream memoryStream = new();
-        using StreamWriter writer = new(memoryStream, Encoding.UTF8);
-        _jsonSerializer.Serialize(writer, type);
-        writer.Flush();
+        using StreamWriter streamWriter = new(memoryStream, _encoding);
+        _jsonSerializer.Serialize(streamWriter, type);
+        streamWriter.Flush();
         return memoryStream.ToArray();
     }
 
     public T Deserialize<T>(byte[] data) where T : class
     {
         using MemoryStream memoryStream = new(data);
-        using StreamReader reader = new(memoryStream, Encoding.UTF8);
-        return (T)_jsonSerializer.Deserialize(reader, typeof(T));
+        using StreamReader streamReader = new(memoryStream, _encoding);
+        return (T)_jsonSerializer.Deserialize(streamReader, typeof(T));
     }
 }
