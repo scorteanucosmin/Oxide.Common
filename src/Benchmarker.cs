f148e54b2a8dc83d7e314ad7a13effb76fa472e6
scorteanucosmin <cozminsc@gmail.com>
2026-02-16 16:46:17 +0200

    Add benchmarker

---
+++ src/Benchmarker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using Oxide.Pooling;
+
+namespace Oxide;
+
+public class Benchmarker : IDisposable
+{
+    private string _input;
+    private Stopwatch _stopwatch;
+
+    public static Benchmarker Start(string input)
+    {
+        Console.WriteLine($"Started benchmarking {input}");
+
+        Benchmarker benchmarker = PoolFactory<Benchmarker>.Shared.Take();
+        benchmarker._input = input;
+        benchmarker._stopwatch = PoolFactory<Stopwatch>.Shared.Take();
+        benchmarker._stopwatch.Start();
+        return benchmarker;
+    }
+
+    public void Dispose()
+    {
+        _stopwatch.Stop();
+
+        Console.WriteLine($"{_input} took {_stopwatch.Elapsed} [{_stopwatch.ElapsedTicks} ticks]");
+
+        PoolFactory<Stopwatch>.Shared.Return(_stopwatch);
+        PoolFactory<Benchmarker>.Shared.Return(this);
+
+        _input = null;
+        _stopwatch = null;
+    }
+}
